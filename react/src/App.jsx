01b3f662e7ef2d2dd36d352e069d8f763a29e37d
Tim Lipps <tlipps@travelers.com>
2024-10-02 15:46:03 -0400

    final react

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {useState, useEffect} from "react";
+import { BrowserRouter as Router,  Route,  Routes,  Link} from "react-router-dom";
+import CharacterName from "../components/CharacterName";
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listOfCharacters, setListOfCharacters] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch(import.meta.env.VITE_CHARACTERS_API_URL);
+            console.log('fetch data - first',response)
+            if (!response.ok) {
+                throw new Error('Data could not be fetched!');
+            }
+            const json_response = await response.json();
+
+            setListOfCharacters(json_response); // assign JSON response to the data variable.
+            
+        } catch (error) {
+            console.error('Error fetching socks:', error);
+        }
+    };
+    
+    fetchData()
+    
+}, []);
+console.log(listOfCharacters)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+    <div>
+    <h1>Characters</h1>
+
+    <CharacterName listOfCharacters={listOfCharacters} />
+    {/* {listOfCharacters[0]?.name} */}
+    </div>
   )
 }
 
