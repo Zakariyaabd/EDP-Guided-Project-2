08d44849cc7c66fe6ea64b422218c0fb6c345905
Zak Abdi <Zakariyamabdi@gmail.com>
2024-10-01 21:35:35 -0400

    mongo connection and characters route

--- Express/server.js
+++ Express/server.js
@@ -1,13 +1,37 @@
 const express = require("express");
+const { MongoClient, ObjectId } = require ("mongodb"); 
+const dotenv = require('dotenv');
+
+// load environment variable from .env
+dotenv.config();
+
+const URL = process.env.MONGO_DB_URL; 
+const dbName = process.env.MONGO_DB; 
+
 const app = express();
 const port = 3000;
 
-app.get("/api/planets", (req, res) =>{
-    res.send("hello world")
+// Middleware to parse JSON bodies
+app.use(express.json());
 
+// mongo DB connections
+app.get('/api/characters', async (req, res) => {
+    
+    try {
+        const client = new MongoClient(URL);
+        await client.connect();
+        const db = client.db(dbName);
+        console.log("Connected to Db ", db);
+        const collection = db.collection('characters'); 
+        console.log("Connected to collection  ", collection);
+        const characters = await collection.find().toArray(); 
+        res.json(characters);
 
+    } catch (error) {
+        console.error("Error fetching: ", error);
+    }
+});
 
-} );
 
 app.listen(port,() =>(console.log("listening")))
 
